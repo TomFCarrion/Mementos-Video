b2b227362c60b774b5baa6ed4e8cdffbbf479811
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-05-15 21:17:53 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const app = document.getElementById('app');
+//ReactDOM.render(que voy a renderizar/donde lo hare);
+const holaMundo = <h1>hola estudiante</h1>;
+ReactDOM.render(holaMundo,app);
