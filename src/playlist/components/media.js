cdc462bdb06537c086633dc8deb1e561739cd8d4
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-05-22 12:12:04 -0300

    Creando componentes en ReactJS
    creando componente Media

---
+++ src/playlist/components/media.js
@@ -0,0 +1,22 @@
+import React,{ Component } from 'react';
+
+class Media extends Component {
+  render() { //UI html
+    return (
+      <div>
+        <div>
+          <img
+            src=""
+            alt=""
+            width={260}
+            height = {160}
+          />
+          <h3>¿Por que aprender React?</h3>
+          <p>TomasCarrion</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Media;
