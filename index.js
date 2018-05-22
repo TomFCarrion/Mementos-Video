e2a590283423f37379d9310c15c7ed88cd73a621
Tomas F. Carrion <tomfcarrion@gmail.com>
2018-05-22 12:58:18 -0300

    Añadiendo estilos a media
    Añadiendo estilos a media component

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import Media from'./src/playlist/components/media';
 
 const app = document.getElementById('app');
 //ReactDOM.render(que voy a renderizar/donde lo hare);
 //const holaMundo = <h1>hola estudiante</h1>;
-ReactDOM.render(<Media /> ,app);
+render(<Media /> ,app);
--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,27 @@
 import React,{ Component } from 'react';
+import './media.css';
 
 class Media extends Component {
   render() { //UI html
+    const styles = {
+      container:{ //los estilos en js se declaran con camelCase
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
     return (
-      <div>
-        <div>
-          <img
-            src=""
+      <div className="Media">
+        <div className="Media-cover">
+          <img className="Media-image"
+            src="./images/covers/bitcoin.jpg"
             alt=""
             width={260}
             height = {160}
           />
-          <h3>¿Por que aprender React?</h3>
-          <p>TomasCarrion</p>
+          <h3 className="Media-title">¿Por que aprender React?</h3>
+          <p className="Media-Author"> TomasCarrion</p>
         </div>
       </div>
     )
